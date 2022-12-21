e671b40f14b9a69a779156c05ff0dbf95618c27c
mushabberul <dev.sabbirmia@gmail.com>
2022-12-21 13:01:08 +0600

    install react in laravel

---
+++ resources/js/pages/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+
+export default function App() {
+    return (
+        <div>This is App</div>
+    )
+}
+
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <App />
+);
